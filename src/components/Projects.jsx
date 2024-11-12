5958ea075fd00e3707710b35329acfb10a2c199c
Adeyemi Fathia <fathiaomolara02@gmail.com>
2024-11-12 13:28:01 +0100

    project update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { FaGithub, FaLink } from 'react-icons/fa6';
 import Carefinder from "../assets/carefinder-img1.png";
 import GithubRepo from "../assets/vue-img1.png";
+import Cryptify from "../assets/cryptify1.png";
 
 function Project() {
   useEffect(() => {
@@ -61,6 +62,30 @@ function Project() {
                 <FaGithub className="text-lg" />
               </Link>
               <a href="https://vue-github-repo-three.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer" >
+                <FaLink className="text-lg" />
+              </a>
+            </span>
+          </div>
+          <div className="projects-container" data-aos="fade-up">
+            <img src={Cryptify} alt="cryptify-img" className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover" />
+            <div>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2 text-black">Cryptify</h1>
+              <p className="mb-3 px-2 text-sm md:text-base text-black">A user-friendly Cryptocurrency Tracker App</p>
+              <span className="flex flex-wrap gap-2 px-2">
+                <p className="works-pill">React</p>
+                <p className="works-pill">Tailwindcss</p>
+                <p className="works-pill">Material UI</p>
+                <p className="works-pill">CoinGecko API</p>
+                
+              </span>
+            </div>
+            <span className='mt-3 mx-2 border-t border-black pt-2 flex items-center justify-between text-black'>
+              <Link to='https://github.com/Faateeha/cryptify' >
+                <FaGithub className="text-lg" />
+              </Link>
+              <a href="https://cryptify-jade.vercel.app/"
           target="_blank"
           rel="noopener noreferrer" >
                 <FaLink className="text-lg" />
