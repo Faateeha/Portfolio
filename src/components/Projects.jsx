b642b497f8abe335f6a010736f27ec225b8a29c0
Adeyemi Fathia <fathiaomolara02@gmail.com>
2025-01-09 17:49:55 +0100

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import { FaGithub, FaLink } from 'react-icons/fa6';
 import Carefinder from "../assets/carefinder-img1.png";
 import GithubRepo from "../assets/vue-img1.png";
 import Cryptify from "../assets/cryptify1.png";
+import Renergy from "../assets/renergy.png"
 
 function Project() {
   useEffect(() => {
@@ -86,6 +87,29 @@ function Project() {
                 <FaGithub className="text-lg" />
               </Link>
               <a href="https://cryptify-jade.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer" >
+                <FaLink className="text-lg" />
+              </a>
+            </span>
+          </div>
+          <div className="projects-container" data-aos="fade-down">
+            <img src={Renergy} alt="renergy-img" className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover" />
+            <div>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2 text-black">Renergy Hub</h1>
+              <p className="mb-3 px-2 text-sm md:text-base text-black">Renergy Hub is a digital marketplace that connects sellers and buyers of renewable energy products, facilitating secure transactions, reliable deliveries, and seamless purchasing.</p>
+              <span className="flex flex-wrap gap-2 px-2">
+                <p className="works-pill">React</p>
+                <p className="works-pill">Tailwindcss</p>
+                <p className="works-pill">Typescript</p>
+                
+              </span>
+            </div>
+            <span className='mt-3 mx-2 border-t border-black pt-2 flex items-center justify-between text-black'>
+              <Link to='https://github.com/InternPulse/renergy-hub-frontend/tree/feature-base ' >
+                <FaGithub className="text-lg" />
+              </Link>
+              <a href="https://www.renergyhub.com.ng/"
           target="_blank"
           rel="noopener noreferrer" >
                 <FaLink className="text-lg" />
